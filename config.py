580a33271d86211d836c52768c4d4e04246f9293
Samuel <83781440+samuelmarc@users.noreply.github.com>
2024-12-07 18:27:41 -0300

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class _Config:
     API_ID = int(os.getenv('API_ID'))
     API_HASH = os.getenv('API_HASH')
     BOT_TOKEN = os.getenv('BOT_TOKEN')
-    WEB_APP = os.getenv('WEB_APP')  # users who can use admin commands (e.g. notify)# Example: https://web-app.com
+    WEB_APP = os.getenv('WEB_APP')  # Example: https://web-app.com
     SUDO_USERS = []  # users who can use admin commands (ex, notify)
     INLINE_CACHE = 5
 
