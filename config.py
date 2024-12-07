b40cf4da7d445347f0c98975abea7dd40898db4a
Samuel <83781440+samuelmarc@users.noreply.github.com>
2024-12-07 18:29:12 -0300

    Update config.py

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ if os.path.exists('.env'):
 
 
 class _Config:
-    API_ID = int(os.getenv('API_ID'))
-    API_HASH = os.getenv('API_HASH')
-    BOT_TOKEN = os.getenv('BOT_TOKEN')
+    API_ID = int(os.getenv('API_ID'))  # https://my.telegram.org/
+    API_HASH = os.getenv('API_HASH')  # https://my.telegram.org/
+    BOT_TOKEN = os.getenv('BOT_TOKEN')  # https://t.me/BotFather
     WEB_APP = os.getenv('WEB_APP')  # Example: https://web-app.com
     SUDO_USERS = []  # users who can use admin commands (ex, notify)
     INLINE_CACHE = 5
